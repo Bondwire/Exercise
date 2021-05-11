9f392fb5a4c039c511c03845deb56b37e63a01b0
Bondwire <68152742+Bondwire@users.noreply.github.com>
2021-05-11 16:24:58 +0200

    return get set zadanie zad 1 metody

---
+++ src/zjava/dzien1/metodyZad1/MainProdukt.java
@@ -0,0 +1,15 @@
+package zjava.dzien1.metodyZad1;
+
+public class MainProdukt {
+    public static void main(String[] args) {
+    Product a = new Product();
+    a.setName("Grzegorz");
+
+        System.out.println(a.getName());
+    }
+}
+
+
+//Utwórz klasę Product, dodaj w niej atrybut name.
+//
+//Dodaj metody setName (ustawiającą name) oraz getName (zwracającą name).
---
+++ src/zjava/dzien1/metodyZad1/Product.java
@@ -0,0 +1,20 @@
+package zjava.dzien1.metodyZad1;
+
+public class Product {
+    private String name;
+
+
+    public void setName(String name) {
+        this.name = name;
+//
+    }
+
+    public String getName() {
+        return ("name wynosi :" + this.name);
+    }
+
+}
+
+//Utwórz klasę Product, dodaj w niej atrybut name.
+//
+//Dodaj metody setName (ustawiającą name) oraz getName (zwracającą name).
